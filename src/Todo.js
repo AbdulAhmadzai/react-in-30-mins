b133782dc7de86635478df052eaaf07f50e0adba
AbdulAhmadzai <61525386+AbdulAhmadzai@users.noreply.github.com>
2020-04-19 20:49:38 +1200

    initial c

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Todo({todo}) {
+    return (
+        <div>
+        <lable>
+            <input type='checkbox' checked={todo.completed} />
+            {todo.name}
+
+
+            </lable>
+        </div>
+    )
+}
